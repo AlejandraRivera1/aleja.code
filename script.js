b80c1d1253454301c65c7c799dfe650cd9c763e2
Alejandra Rivera <riverarinconalejandra1@gmail.com>
2024-10-18 04:53:29 -0500

    diseño responsivo completado, sin proyectos

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-//llamado a la accion del boton Menu
-
 document.addEventListener("DOMContentLoaded", function() {
     const menuButton = document.querySelector('.menu-button');
     const menuList = document.querySelector('.nav-right-section');
-    const menuItems = menuList.querySelectorAll('li'); // Selecciona todos los elementos de la lista
+    const menuItems = menuList.querySelectorAll('li'); 
+
+    // Inicialmente ocultar el menú en pantallas pequeñas
+    if (window.innerWidth <= 480) {
+        menuList.style.display = 'none';
+    }
 
     menuButton.addEventListener('click', function() {
-        // Alternar la visibilidad de la lista del menú
         if (menuList.style.display === 'none' || menuList.style.display === '') {
-            menuList.style.display = 'flex'; // Muestra el menú
+            menuList.style.display = 'flex'; 
         } else {
-            menuList.style.display = 'none'; // Oculta el menú
+            menuList.style.display = 'none';
         }
     });
 
-    // Agregar un evento click a cada elemento de la lista
     menuItems.forEach(item => {
-        item.addEventListener('click', function() {
-            menuList.style.display = 'none'; // Oculta el menú al seleccionar una opción
+        item.addEventListener("click", function () {
+            // Oculta el menú solo si la pantalla es pequeña
+            if (window.innerWidth <= 480) {
+                menuList.style.display = "none";
+            }
+
+            // Desplaza la página hacia la sección correspondiente
+            const targetId = this.querySelector("a").getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+            const headerHeight = 100; // Altura del header
+            const offsetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: "smooth" // Desplazamiento suave
+            });
         });
     });
 });
